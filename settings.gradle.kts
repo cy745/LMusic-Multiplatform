08e0a62fc09d77237cc94ac64980f90206f53182
cy745 <1248393538@qq.com>
2023-06-19 11:02:13 +0800

    代码统一格式化，TAB宽度为两个空格

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,33 +5,33 @@ include(":shared")
 include(":desktopApp")
 
 pluginManagement {
-    repositories {
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-    }
+  repositories {
+    maven("https://maven.aliyun.com/repository/gradle-plugin")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/central")
+  }
 
-    plugins {
-        val kotlinVersion = extra["kotlin.version"] as String
-        val agpVersion = extra["agp.version"] as String
-        val composeVersion = extra["compose.version"] as String
+  plugins {
+    val kotlinVersion = extra["kotlin.version"] as String
+    val agpVersion = extra["agp.version"] as String
+    val composeVersion = extra["compose.version"] as String
 
-        kotlin("jvm").version(kotlinVersion)
-        kotlin("multiplatform").version(kotlinVersion)
-        kotlin("android").version(kotlinVersion)
+    kotlin("jvm").version(kotlinVersion)
+    kotlin("multiplatform").version(kotlinVersion)
+    kotlin("android").version(kotlinVersion)
 
-        id("com.android.application").version(agpVersion)
-        id("com.android.library").version(agpVersion)
+    id("com.android.application").version(agpVersion)
+    id("com.android.library").version(agpVersion)
 
-        id("org.jetbrains.compose").version(composeVersion)
-    }
+    id("org.jetbrains.compose").version(composeVersion)
+  }
 }
 
 dependencyResolutionManagement {
-    repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  repositories {
+    maven("https://maven.aliyun.com/repository/google")
+    maven("https://maven.aliyun.com/repository/central")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  }
 }
